452782331bbbe2cad8264d86ff63d8013f59f930
Erwin Klaver <97507981+superKlaver-nl@users.noreply.github.com>
2023-09-19 13:11:27 +0200

    Delete Autopilot directory

--- Autopilot/start.cmd
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-cmd.exe /c powershell.exe -NoLogo -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/gemeentewestland-nl/intune-public/main/Autopilot/RunWindowsAutoPilotInfo.ps1 -OutFile C:\Windows\Temp\RunWindowsAutoPilotInfo.ps1"
-
-cmd.exe /c powershell.exe -executionpolicy bypass -file C:\Windows\Temp\RunWindowsAutoPilotInfo.ps1
